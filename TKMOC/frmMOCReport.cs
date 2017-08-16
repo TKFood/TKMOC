9e3899af196934b5010486234bc0b15f6cbb485f
at3158 <tk290@tkfood.com.tw>
2017-08-16 11:18:26 +0800

    ADD 烘烤製程記錄

--- TKMOC/frmMOCReport.cs
+++ TKMOC/frmMOCReport.cs
@@ -914,6 +914,13 @@ namespace TKMOC
                 //report1.Load(@"REPORT\TEST1.frx");
 
             }
+            else if (comboBox2.Text.Equals("烘烤製程記錄"))
+            {
+                report1 = new Report();
+                report1.Load(@"REPORT\烘烤製程記錄.frx");
+                //report1.Load(@"REPORT\TEST1.frx");
+
+            }
 
 
             report1.SetParameterValue("P1", dateTimePicker3.Value.ToString("yyyyMMdd"));
