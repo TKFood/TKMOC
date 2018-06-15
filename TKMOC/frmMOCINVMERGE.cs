68ff1b66e52a7fae59362fe44cced5cfba7668aa
at3158 <tk290@tkfood.com.tw>
2018-06-15 14:40:18 +0800

    DataGridViewColumn

--- TKMOC/frmMOCINVMERGE.cs
+++ TKMOC/frmMOCINVMERGE.cs
@@ -33,11 +33,11 @@ namespace TKMOC
         string connectionString;
         StringBuilder sbSql = new StringBuilder();
         StringBuilder sbSqlQuery = new StringBuilder();
-        SqlDataAdapter adapter = new SqlDataAdapter();
-        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter1 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
         SqlTransaction tran;
         SqlCommand cmd = new SqlCommand();
-        DataSet ds = new DataSet();
+        DataSet ds1 = new DataSet();
         DataSet ds2 = new DataSet();
         string tablename = null;
         int rownum = 0;
@@ -50,16 +50,102 @@ namespace TKMOC
         public frmMOCINVMERGE()
         {
             InitializeComponent();
+
+            combobox2load();
         }
 
 
         #region FUNCTION
+        public void combobox2load()
+        {
+
+            connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+            sqlConn = new SqlConnection(connectionString);
+            String Sequel = "SELECT MD002,MD001 FROM [TK].dbo.CMSMD WHERE MD002 LIKE '新%' ORDER BY MD001";
+            SqlDataAdapter da = new SqlDataAdapter(Sequel, sqlConn);
+            DataTable dt = new DataTable();
+            sqlConn.Open();
+
+            dt.Columns.Add("MD001", typeof(string));
+            dt.Columns.Add("MD002", typeof(string));
+            da.Fill(dt);
+            comboBox2.DataSource = dt.DefaultView;
+            comboBox2.ValueMember = "MD001";
+            comboBox2.DisplayMember = "MD002";
+            sqlConn.Close();
+
+
+
+        }
+        public void SERACH()
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"  SELECT TC001 AS '領料單',TC002 AS '單號',TC005  AS '線別' ");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.MOCTC");
+                sbSql.AppendFormat(@"  WHERE TC003>='{0}' AND TC003<='{1}'",dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                sbSql.AppendFormat(@"  AND TC005='{0}'",comboBox2.SelectedValue.ToString());
+                sbSql.AppendFormat(@"  ORDER BY TC001,TC002,TC005");
+                sbSql.AppendFormat(@"  ");
+
+                adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder1 = new SqlCommandBuilder(adapter1);
+                sqlConn.Open();
+                ds1.Clear();
+                adapter1.Fill(ds1, "TEMPds1");
+                sqlConn.Close();
+
+
+                if (ds1.Tables["TEMPds1"].Rows.Count == 0)
+                {
+
+                }
+                else
+                {
+                    if (ds1.Tables["TEMPds1"].Rows.Count >= 1)
+                    {
+                        //dataGridView1.Rows.Clear();
+                        dataGridView1.DataSource = ds1.Tables["TEMPds1"];
+
+                        DataGridViewColumn dgvc = new DataGridViewCheckBoxColumn();
+                        dgvc.Width = 60;
+                        dgvc.Name = "選取";
+
+
+                        //新增到DataGridView內的第0欄
+                        this.dataGridView1.Columns.Insert(0, dgvc);
+
+                        dataGridView1.AutoResizeColumns();
+                        //dataGridView1.CurrentCell = dataGridView1[0, rownum];
+
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
 
         #endregion
 
         #region BUTTON
         private void button1_Click(object sender, EventArgs e)
         {
+            SERACH();
 
         }
 
