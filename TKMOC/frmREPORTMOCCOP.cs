c33b71fa96a0e3c3593d10c161af93d7897bf7ff
at3158 <tk290@tkfood.com.tw>
2019-04-25 10:07:03 +0800

    SETSQL1

--- TKMOC/frmREPORTMOCCOP.cs
+++ TKMOC/frmREPORTMOCCOP.cs
@@ -71,8 +71,8 @@ namespace TKMOC
             SB.AppendFormat(" ,(SELECT ISNULL(SUM(TA017),0) FROM [TK].dbo.MOCTA A WHERE A.TA011 IN ('Y','y') AND A.TA026=[COPTD].TD001 AND A.TA027=[COPTD].TD002 AND A.TA028=[COPTD].TD003 AND A.TA006=[COPTD].TD004  ) AS '訂單總生產量'");
             SB.AppendFormat(" ,ISNULL(((SELECT ISNULL(SUM(TA017),0) FROM [TK].dbo.MOCTA A WHERE A.TA011 IN ('Y','y') AND A.TA026=[COPTD].TD001 AND A.TA027=[COPTD].TD002 AND A.TA028=[COPTD].TD003 AND A.TA006=[COPTD].TD004  ) -[OLDNUM]),0) AS '生產數量是否滿足訂單'");
             SB.AppendFormat(" ,[COPTD].TD013 AS '訂單預交日'");
-            SB.AppendFormat(" ,DATEDIFF (DAY,[COPTD].TD013,TA014) AS '是否延遲訂單預交'");
-            SB.AppendFormat(" ,DATEDIFF (DAY,TA010,TA014) AS '是否延遲製令完工'");
+            SB.AppendFormat(" ,CASE WHEN ISNULL(TA014,'')<>'' THEN DATEDIFF (DAY,[COPTD].TD013,TA014) ELSE 999 END AS '是否延遲訂單預交'");
+            SB.AppendFormat(" ,CASE WHEN ISNULL(TA014,'')<>'' THEN DATEDIFF (DAY,TA010,TA014) ELSE 999 END  AS '是否延遲製令完工'");
             SB.AppendFormat(" ,ISNULL((TA017-TA015),0) AS '製令生產數量生否>預計生產'");
             SB.AppendFormat(" FROM [TK].dbo.MOCTA");
             SB.AppendFormat(" LEFT JOIN [TK].[dbo].[VCOPTDINVMD] ON [VCOPTDINVMD].TD001=TA026 AND [VCOPTDINVMD].TD002=TA027 AND [VCOPTDINVMD].TD003=TA028");
