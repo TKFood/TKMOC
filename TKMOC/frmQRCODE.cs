b2c59d94585ec024e6a4187963c410a8e1bbbb2a
at3158 <tk290@tkfood.com.tw>
2019-09-11 15:59:09 +0800

    FASTSQL

--- TKMOC/frmQRCODE.cs
+++ TKMOC/frmQRCODE.cs
@@ -145,7 +145,7 @@ namespace TKMOC
             FASTSQL.AppendFormat(@"  AND [EmployeeState].[Name]='{0}'", comboBox3.Text);
             FASTSQL.AppendFormat(@"  ORDER BY [Employee].[Code]");
             FASTSQL.AppendFormat(@"    ");
-            FASTSQL.AppendFormat(@"  ");
+            FASTSQL.AppendFormat(@"   ");
 
             return FASTSQL.ToString();
         }
