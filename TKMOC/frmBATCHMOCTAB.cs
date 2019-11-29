1c947e3fb19bbf745cc159efe5cf0645a1ab10c3
at3158 <tk290@tkfood.com.tw>
2019-11-29 10:53:23 +0800

    GENADDTARGET

--- TKMOC/frmBATCHMOCTAB.cs
+++ TKMOC/frmBATCHMOCTAB.cs
@@ -32,11 +32,14 @@ namespace TKMOC
         SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
         SqlDataAdapter adapter2 = new SqlDataAdapter();
         SqlCommandBuilder sqlCmdBuilder2 = new SqlCommandBuilder();
+        SqlDataAdapter adapter3= new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder3 = new SqlCommandBuilder();
 
         SqlTransaction tran;
         SqlCommand cmd = new SqlCommand();
         DataSet ds1 = new DataSet();
         DataSet ds2 = new DataSet();
+        DataSet ds3 = new DataSet();
 
         List<ADDITEM> ADDTARGET = new List<ADDITEM>();
         List<ADDITEM> FIND = new List<ADDITEM>();
@@ -53,135 +56,7 @@ namespace TKMOC
             InitializeComponent();
         }
 
-        public void TEST()
-        {
-            ADDTARGET.Clear();
-            ADDTARGET.Add(new ADDITEM { MB001 = "40101110430280", NUM =100 });
-
-            SERACH(ADDTARGET[0].MB001, ADDTARGET[0].NUM);
-
-            foreach (var find in FIND)
-            {
-                CHECKBOMMD(find.MB001, find.NUM);                
-            }
-
-            foreach (var find in ADDTARGET)
-            {
-               MessageBox.Show(find.MB001 + " " + find.NUM);
-            }
-
-        }
-
-        public void SERACH(string MB001,double NUM)
-        {
-            try
-            {
-                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
-                sqlConn = new SqlConnection(connectionString);
-
-                sbSql.Clear();
-                sbSqlQuery.Clear();
-
-                sbSql.AppendFormat(@"  WITH NODE (MD001,MD003,LAYER,MD004,MC004,PREMC004,MD006,MD007,MD008,USEDNUM ,PREUSEDNUM) AS");
-                sbSql.AppendFormat(@"  (");
-                sbSql.AppendFormat(@"  SELECT MD001,MD003,0 ,[MD004],[MC004],[MC004] AS PREMC004,[MD006],[MD007],[MD008],CONVERT(DECIMAL(18,4),([MD006]/[MD007]/[MC004]*(1+MD008))),CONVERT(DECIMAL(18,4),1) AS PREUSEDNUM  FROM [TK].[dbo].[VBOMMD]");
-                sbSql.AppendFormat(@"  UNION ALL");
-                sbSql.AppendFormat(@"  SELECT TB1.MD001,TB2.MD003,TB2.LAYER+1,TB2.MD004,TB2.MC004,TB1.MC004,TB2.MD006,TB2.MD007,TB2.MD008,TB2.USEDNUM,CONVERT(DECIMAL(18,4),(TB1.[MD006]/TB1.[MD007]/TB1.[MC004]*(1+TB1.MD008))) AS PREUSEDNUM FROM [TK].[dbo].[VBOMMD] TB1");
-                sbSql.AppendFormat(@"  INNER JOIN NODE TB2");
-                sbSql.AppendFormat(@"  ON TB1.MD003 = TB2.MD001");
-                sbSql.AppendFormat(@"  )");
-                sbSql.AppendFormat(@"  SELECT MD001,MD003,LAYER,MD004,MC004,PREMC004,MD006,MD007,MD008,USEDNUM ,PREUSEDNUM ,USEDNUM*PREUSEDNUM*{0} AS TOTALUSED FROM NODE",NUM);
-                sbSql.AppendFormat(@"  WHERE  MD001='{0}'",MB001);
-                sbSql.AppendFormat(@"  ORDER BY LAYER ,MD001, MD003");
-                sbSql.AppendFormat(@"  ");
-                sbSql.AppendFormat(@"  ");
-                sbSql.AppendFormat(@"  ");
-
-                adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
-
-                sqlCmdBuilder1 = new SqlCommandBuilder(adapter1);
-                sqlConn.Open();
-                ds1.Clear();
-                adapter1.Fill(ds1, "ds1");
-                sqlConn.Close();
-
-
-                if (ds1.Tables["ds1"].Rows.Count == 0)
-                {
-                    
-                }
-                else
-                {
-                    if (ds1.Tables["ds1"].Rows.Count >= 1)
-                    {
-                        foreach (DataRow od in ds1.Tables["ds1"].Rows)
-                        {
-                            FIND.Add(new ADDITEM { MB001 = od["MD003"].ToString(), NUM =Convert.ToDouble(od["TOTALUSED"].ToString()) });
-                        }
-                        
-                    }
-                }
-
-            }
-            catch
-            {
-
-            }
-            finally
-            {
-
-            }
-        }
-
-        public void CHECKBOMMD(string MB001,double NUM)
-        {
-            try
-            {
-                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
-                sqlConn = new SqlConnection(connectionString);
-
-                sbSql.Clear();
-                sbSqlQuery.Clear();
-
-                sbSql.AppendFormat(@"  SELECT MD001,MD003");
-                sbSql.AppendFormat(@"  FROM [TK].dbo.BOMMD");
-                sbSql.AppendFormat(@"  WHERE MD001='{0}'",MB001);
-                sbSql.AppendFormat(@"  ");
-                sbSql.AppendFormat(@"  ");
-                sbSql.AppendFormat(@"  ");
-
-                adapter2 = new SqlDataAdapter(@"" + sbSql, sqlConn);
-
-                sqlCmdBuilder2 = new SqlCommandBuilder(adapter2);
-                sqlConn.Open();
-                ds2.Clear();
-                adapter2.Fill(ds2, "ds2");
-                sqlConn.Close();
-
-
-                if (ds2.Tables["ds2"].Rows.Count == 0)
-                {
-
-                }
-                else
-                {
-                    if (ds2.Tables["ds2"].Rows.Count >= 1)
-                    {
-                        ADDTARGET.Add(new ADDITEM { MB001 = MB001, NUM = NUM });
-                        
-                    }
-                }
-
-            }
-            catch
-            {
-
-            }
-            finally
-            {
-
-            }
-        }
+        
 
 
 
@@ -282,6 +157,140 @@ namespace TKMOC
                 }
             }
         }
+
+        public void GENADDTARGET()
+        {
+            ADDTARGET.Clear();
+            FIND.Clear();
+            //ADDTARGET.RemoveAll(it => true);
+
+            ADDTARGET.Add(new ADDITEM { MB001 =textBox4.Text , NUM = Convert.ToDouble(textBox5.Text) });
+
+            SERACH(ADDTARGET[0].MB001, ADDTARGET[0].NUM);
+
+            foreach (var find in FIND)
+            {
+                CHECKBOMMD(find.MB001, find.NUM);
+            }
+
+            foreach (var find in ADDTARGET)
+            {
+                MessageBox.Show(find.MB001 + " " + find.NUM);
+            }
+
+        }
+
+        public void SERACH(string MB001, double NUM)
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"  WITH NODE (MD001,MD003,LAYER,MD004,MC004,PREMC004,MD006,MD007,MD008,USEDNUM ,PREUSEDNUM) AS");
+                sbSql.AppendFormat(@"  (");
+                sbSql.AppendFormat(@"  SELECT MD001,MD003,0 ,[MD004],[MC004],[MC004] AS PREMC004,[MD006],[MD007],[MD008],CONVERT(DECIMAL(18,4),([MD006]/[MD007]/[MC004]*(1+MD008))),CONVERT(DECIMAL(18,4),1) AS PREUSEDNUM  FROM [TK].[dbo].[VBOMMD]");
+                sbSql.AppendFormat(@"  UNION ALL");
+                sbSql.AppendFormat(@"  SELECT TB1.MD001,TB2.MD003,TB2.LAYER+1,TB2.MD004,TB2.MC004,TB1.MC004,TB2.MD006,TB2.MD007,TB2.MD008,TB2.USEDNUM,CONVERT(DECIMAL(18,4),(TB1.[MD006]/TB1.[MD007]/TB1.[MC004]*(1+TB1.MD008))) AS PREUSEDNUM FROM [TK].[dbo].[VBOMMD] TB1");
+                sbSql.AppendFormat(@"  INNER JOIN NODE TB2");
+                sbSql.AppendFormat(@"  ON TB1.MD003 = TB2.MD001");
+                sbSql.AppendFormat(@"  )");
+                sbSql.AppendFormat(@"  SELECT MD001,MD003,LAYER,MD004,MC004,PREMC004,MD006,MD007,MD008,USEDNUM ,PREUSEDNUM ,USEDNUM*PREUSEDNUM*{0} AS TOTALUSED FROM NODE", NUM);
+                sbSql.AppendFormat(@"  WHERE  MD001='{0}'", MB001);
+                sbSql.AppendFormat(@"  ORDER BY LAYER ,MD001, MD003");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ");
+
+                adapter2 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder2 = new SqlCommandBuilder(adapter2);
+                sqlConn.Open();
+                ds2.Clear();
+                adapter2.Fill(ds2, "ds2");
+                sqlConn.Close();
+
+
+                if (ds2.Tables["ds2"].Rows.Count == 0)
+                {
+
+                }
+                else
+                {
+                    if (ds2.Tables["ds2"].Rows.Count >= 1)
+                    {
+                        foreach (DataRow od in ds2.Tables["ds2"].Rows)
+                        {
+                            FIND.Add(new ADDITEM { MB001 = od["MD003"].ToString(), NUM = Convert.ToDouble(od["TOTALUSED"].ToString()) });
+                        }
+
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        public void CHECKBOMMD(string MB001, double NUM)
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"  SELECT MD001,MD003");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.BOMMD");
+                sbSql.AppendFormat(@"  WHERE MD001='{0}'", MB001);
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ");
+
+                adapter3 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder3 = new SqlCommandBuilder(adapter3);
+                sqlConn.Open();
+                ds3.Clear();
+                adapter3.Fill(ds3, "ds3");
+                sqlConn.Close();
+
+
+                if (ds3.Tables["ds3"].Rows.Count == 0)
+                {
+
+                }
+                else
+                {
+                    if (ds3.Tables["ds3"].Rows.Count >= 1)
+                    {
+                        ADDTARGET.Add(new ADDITEM { MB001 = MB001, NUM = NUM });
+
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
         #endregion
 
         #region BUTTON
@@ -291,7 +300,7 @@ namespace TKMOC
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            TEST();
+            GENADDTARGET();
         }
 
         #endregion
