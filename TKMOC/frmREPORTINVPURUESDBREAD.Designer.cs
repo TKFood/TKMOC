b553e4ffdcb390352206811d7bc27d2173c958af
at3158 <tk290@tkfood.com.tw>
2020-05-21 14:34:51 +0800

    SEARCHINVPURMOC

--- TKMOC/frmREPORTINVPURUESDBREAD.Designer.cs
+++ TKMOC/frmREPORTINVPURUESDBREAD.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
@@ -281,8 +281,8 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
@@ -290,13 +290,14 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(493, 492);
             this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // dataGridView2
             // 
             this.dataGridView2.AllowUserToAddRows = false;
             this.dataGridView2.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView2.Location = new System.Drawing.Point(0, 0);
--- TKMOC/frmREPORTINVPURUESDBREAD.cs
+++ TKMOC/frmREPORTINVPURUESDBREAD.cs
@@ -75,7 +75,7 @@ namespace TKMOC
                 sbSql.AppendFormat(@"  FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB");
                 sbSql.AppendFormat(@"  WHERE TA001=TB001 AND TA002=TB002");
                 sbSql.AppendFormat(@"  AND TA021='04'");
-                sbSql.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{0}'", SDay, EDay);
+                sbSql.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{1}'", SDay, EDay);
                 sbSql.AppendFormat(@"  GROUP BY TB003,TB012");
                 sbSql.AppendFormat(@"  ORDER BY TB003,TB012");
                 sbSql.AppendFormat(@"  ");
@@ -128,7 +128,166 @@ namespace TKMOC
 
         }
 
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
 
+            if (dataGridView1.CurrentRow != null)
+            {
+                int rowindex = dataGridView1.CurrentRow.Index;
+                if (rowindex >= 0)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[rowindex];
+                    MD003 = row.Cells["品號"].Value.ToString().Trim();
+
+                    SEARCHINVPURMOC(MD003, dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+
+
+                }
+                else
+                {
+                    MD003 = null;
+                }
+            }
+        }
+
+        public void SEARCHINVPURMOC(string MD003, string SDay, string EDay)
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+
+                //查每日原物料的使用量、進貨量、庫存量
+                //另外查詢領料數量
+                //sbSql.AppendFormat(@"  ,ISNULL((SELECT SUM(TE005) FROM [TK].dbo.MOCTE WHERE TE011+TE012 IN ( SELECT TA001+TA002 FROM [TK].dbo.MOCTA WHERE TE004=TEMP2.MD003 AND TA026=TEMP2.COPTD001 AND TA027=TEMP2.COPTD002 AND TA028=TEMP2.COPTD003 )),0)*-1 AS '領料數量' ");
+
+                sbSql.AppendFormat(@"  SELECT SUM(TEMP4.TNUM) AS '預計庫存量',TEMP2.ID AS '列數',TEMP2.MANU AS '線別',TEMP2.MANUDATE AS '日期',TEMP2.MD003 AS '品號',TEMP2.MD035 AS '品名',TEMP2.TNUM AS '用量'");
+
+                sbSql.AppendFormat(@"  ,TEMP2.MB004 AS '單位',TEMP2.MB001 AS '成品',TEMP2.MB002 AS '成品名',TEMP2.PACKAGE AS '成品數',TEMP2.COPTD001 AS '訂單單別',TEMP2.COPTD002 AS '訂單單號',TEMP2.COPTD003 AS '訂單序號' ");
+                sbSql.AppendFormat(@"  FROM (");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  SELECT ROW_NUMBER() OVER (ORDER BY TEMP.MANUDATE) AS ID,MANU,MANUDATE,MD003,MD035,TNUM,MB004,MB001,MB002,PACKAGE,COPTD001,COPTD002,COPTD003");
+                sbSql.AppendFormat(@"  FROM (");
+                sbSql.AppendFormat(@"  SELECT MD002 AS MANU ,TA003 AS MANUDATE ,TB003 AS MD003,TB012 AS MD035,TB004 AS TNUM,TB007 AS MB004,TA006 AS MB001,TA034 AS MB002,TA015 AS PACKAGE,TA026 AS COPTD001,TA027 AS COPTD002,TA028 COPTD003");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB,[TK].dbo.CMSMD");
+                sbSql.AppendFormat(@"  WHERE TA001=TB001 AND TA002=TB002");
+                sbSql.AppendFormat(@"  AND MD001=TA021");
+                sbSql.AppendFormat(@"  AND TA021='04'");
+                sbSql.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{1}'",SDay,EDay);
+                sbSql.AppendFormat(@"  AND TB003='{0}'", MD003);           
+                sbSql.AppendFormat(@"  UNION");
+                sbSql.AppendFormat(@"  SELECT '1進貨',TD012,TD004,MB002,CONVERT(DECIMAL(14,2),(CASE WHEN ISNULL(MD002,'')<>'' THEN (ISNULL(TD008-TD015,0)*MD004/MD003) ELSE (TD008-TD015) END )) ,MB004,NULL,NULL,NULL,TD001,TD002,TD003");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.INVMB,[TK].dbo.PURTC,[TK].dbo.PURTD ");
+                sbSql.AppendFormat(@"  LEFT JOIN [TK].dbo.INVMD ON MD001=TD004 AND MD002=TD009  ");
+                sbSql.AppendFormat(@"  WHERE TC001=TD001 AND TC002=TD002 AND TD004=MB001 AND TD018='Y' AND TD016='N'");
+                sbSql.AppendFormat(@"  AND TD012>='{0}' AND TD012<='{1}' ", SDay, EDay);
+                sbSql.AppendFormat(@"  AND TD004='{0}'", MD003);
+                sbSql.AppendFormat(@"  UNION ");
+                sbSql.AppendFormat(@"  SELECT '0庫存' AS MANU,CONVERT(NVARCHAR,GETDATE(),112) AS MANUDATE,LA001 AS MD003,MB002,SUM(LA005*LA011) TNUM, MB004,NULL AS MB001,NULL AS MB002,NULL AS PACKAGE,NULL AS COPTD001,NULL AS COPTD002,NULL AS COPTD002");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.INVLA,[TK].dbo.INVMB");
+                sbSql.AppendFormat(@"  WHERE LA001=MB001");
+                sbSql.AppendFormat(@"  AND  LA009 IN ('20004','20006' ) AND LA004<=CONVERT(NVARCHAR,GETDATE(),112) ");
+                sbSql.AppendFormat(@"  AND LA001='{0}' ", MD003);
+                sbSql.AppendFormat(@"  GROUP BY LA001,MB002,MB004");
+                sbSql.AppendFormat(@"  UNION");
+                sbSql.AppendFormat(@"  SELECT '1手動進貨',CONVERT(NVARCHAR,INVPURUESD.DATES,112),INVPURUESD.MB001,MB002,NUM ,MB004,NULL,NULL,NULL,NULL,NULL,NULL");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.INVMB,[TKMOC].dbo.INVPURUESD ");
+                sbSql.AppendFormat(@"  WHERE INVMB.MB001=INVPURUESD.MB001");
+                sbSql.AppendFormat(@"  AND INVPURUESD.DATES>='{0}' AND INVPURUESD.DATES<='{1}'", SDay, EDay);
+                sbSql.AppendFormat(@"  AND INVPURUESD.MB001='{0}'", MD003);
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ) AS TEMP ");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ) AS TEMP2 JOIN ");
+                sbSql.AppendFormat(@"  (SELECT ROW_NUMBER() OVER (ORDER BY TEMP3.MANUDATE) AS ID,MANU,MANUDATE,MD003,MD035,TNUM,MB004,MB001,MB002,PACKAGE,COPTD001,COPTD002,COPTD003");
+                sbSql.AppendFormat(@"  FROM (");
+                sbSql.AppendFormat(@"  SELECT MD002 AS MANU ,TA003 AS MANUDATE ,TB003 AS MD003,TB012 AS MD035,TB004 AS TNUM,TB007 AS MB004,TA006 AS MB001,TA034 AS MB002,TA015 AS PACKAGE,TA026 AS COPTD001,TA027 AS COPTD002,TA028 COPTD003");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB,[TK].dbo.CMSMD");
+                sbSql.AppendFormat(@"  WHERE TA001=TB001 AND TA002=TB002");
+                sbSql.AppendFormat(@"  AND MD001=TA021");
+                sbSql.AppendFormat(@"  AND TA021='04'");
+                sbSql.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{1}'", SDay, EDay);
+                sbSql.AppendFormat(@"  AND TB003='{0}'", MD003);   
+                sbSql.AppendFormat(@"  UNION");
+                sbSql.AppendFormat(@"  SELECT '1進貨',TD012,TD004,MB002,CONVERT(DECIMAL(14,2),(CASE WHEN ISNULL(MD002,'')<>'' THEN (ISNULL(TD008-TD015,0)*MD004/MD003) ELSE (TD008-TD015) END )) ,MB004,NULL,NULL,NULL,TD001,TD002,TD003");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.INVMB,[TK].dbo.PURTC,[TK].dbo.PURTD ");
+                sbSql.AppendFormat(@"  LEFT JOIN [TK].dbo.INVMD ON MD001=TD004 AND MD002=TD009 ");
+                sbSql.AppendFormat(@"  WHERE TC001=TD001 AND TC002=TD002 AND TD004=MB001 AND TD018='Y' AND TD016='N'");
+                sbSql.AppendFormat(@"  AND TD012>='{0}' AND TD012<='{1}' ", SDay, EDay);
+                sbSql.AppendFormat(@"  AND TD004='{0}'", MD003);
+                sbSql.AppendFormat(@"  UNION ");
+                sbSql.AppendFormat(@"  SELECT '0庫存' AS MANU,CONVERT(NVARCHAR,GETDATE(),112) AS MANUDATE,LA001 AS MD003,MB002,SUM(LA005*LA011) TNUM, MB004,NULL AS MB001,NULL AS MB002,NULL AS PACKAGE,NULL AS COPTD001,NULL AS COPTD002,NULL AS COPTD002");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.INVLA,[TK].dbo.INVMB");
+                sbSql.AppendFormat(@"  WHERE LA001=MB001");
+                sbSql.AppendFormat(@"  AND  LA009 IN ('20004','20006' ) AND LA004<=CONVERT(NVARCHAR,GETDATE(),112) ");
+                sbSql.AppendFormat(@"  AND LA001='{0}' ", MD003);
+                sbSql.AppendFormat(@"  GROUP BY LA001,MB002,MB004");
+                sbSql.AppendFormat(@"  UNION");
+                sbSql.AppendFormat(@"  SELECT '1手動進貨',CONVERT(NVARCHAR,INVPURUESD.DATES,112),INVPURUESD.MB001,MB002,NUM ,MB004,NULL,NULL,NULL,NULL,NULL,NULL");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.INVMB,[TKMOC].dbo.INVPURUESD ");
+                sbSql.AppendFormat(@"  WHERE INVMB.MB001=INVPURUESD.MB001");
+                sbSql.AppendFormat(@"  AND INVPURUESD.DATES>='{0}' AND INVPURUESD.DATES<='{1}'", SDay, EDay);
+                sbSql.AppendFormat(@"  AND INVPURUESD.MB001='{0}'", MD003);
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ) AS TEMP3");
+                sbSql.AppendFormat(@"  ) AS TEMP4 ON TEMP2.ID>=TEMP4.ID");
+                sbSql.AppendFormat(@"  GROUP BY TEMP2.ID,TEMP2.MANU,TEMP2.MANUDATE,TEMP2.MD003,TEMP2.MD035,TEMP2.TNUM,TEMP2.MB004,TEMP2.MB001,TEMP2.MB002,TEMP2.PACKAGE,TEMP2.COPTD001,TEMP2.COPTD002,TEMP2.COPTD003");
+                sbSql.AppendFormat(@"  ORDER BY TEMP2.MANUDATE, TEMP2.MANU");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@"  ");
+
+                adapter2 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder2 = new SqlCommandBuilder(adapter2);
+                sqlConn.Open();
+                ds2.Clear();
+                adapter2.Fill(ds2, "ds2");
+                sqlConn.Close();
+
+
+                if (ds2.Tables["ds2"].Rows.Count == 0)
+                {
+                    dataGridView2.DataSource = null;
+                }
+                else
+                {
+                    if (ds2.Tables["ds2"].Rows.Count >= 1)
+                    {
+                        //dataGridView1.Rows.Clear();
+                        dataGridView2.DataSource = ds2.Tables["ds2"];
+                        dataGridView2.AutoResizeColumns();
+                        //dataGridView1.CurrentCell = dataGridView1[0, rownum];
+
+                        //根据列表中数据不同，显示不同颜色背景
+                        foreach (DataGridViewRow dgRow in dataGridView2.Rows)
+                        {
+                            //判断
+                            if (Convert.ToDecimal(dgRow.Cells[5].Value) > 0)
+                            {
+                                //将这行的背景色设置成Pink
+                                dgRow.DefaultCellStyle.BackColor = Color.Pink;
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
         #endregion
 
         #region BUTTON
@@ -139,5 +298,6 @@ namespace TKMOC
 
         #endregion
 
+       
     }
 }
