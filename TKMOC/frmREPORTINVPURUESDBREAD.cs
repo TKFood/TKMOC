4eb9ee387a4102aada31041521e70c7734c1598c
at3158 <tk290@tkfood.com.tw>
2020-05-21 13:05:03 +0800

    SEARCHMOCMANULINE

--- TKMOC/frmREPORTINVPURUESDBREAD.cs
+++ TKMOC/frmREPORTINVPURUESDBREAD.cs
@@ -59,13 +59,82 @@ namespace TKMOC
         }
 
         #region FUNCTION
+        public void SEARCHMOCMANULINE(string SDay, string EDay)
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"  SELECT TB003 AS '品號',TB012  AS '品名',SUM(TB004)  AS '需領用量'");
+                sbSql.AppendFormat(@"  ,(SELECT ISNULL(SUM(LA005*LA011),0) FROM [TK].dbo.INVLA WHERE LA009 IN ('20004','20006') AND  LA001=TB003) AS '原料倉庫存'");
+                sbSql.AppendFormat(@"  ,((SELECT ISNULL(SUM(LA005*LA011),0) FROM [TK].dbo.INVLA WHERE LA009 IN ('20004','20006') AND  LA001=TB003)-SUM(TB004)) AS '差異量'");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.MOCTA,[TK].dbo.MOCTB");
+                sbSql.AppendFormat(@"  WHERE TA001=TB001 AND TA002=TB002");
+                sbSql.AppendFormat(@"  AND TA021='04'");
+                sbSql.AppendFormat(@"  AND TA003>='{0}' AND TA003<='{0}'", SDay, EDay);
+                sbSql.AppendFormat(@"  GROUP BY TB003,TB012");
+                sbSql.AppendFormat(@"  ORDER BY TB003,TB012");
+                sbSql.AppendFormat(@"  ");
+
+                adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder1 = new SqlCommandBuilder(adapter1);
+                sqlConn.Open();
+                ds1.Clear();
+                adapter1.Fill(ds1, "ds1");
+                sqlConn.Close();
+
+
+                if (ds1.Tables["ds1"].Rows.Count == 0)
+                {
+                    dataGridView1.DataSource = null;
+                }
+                else
+                {
+                    if (ds1.Tables["ds1"].Rows.Count >= 1)
+                    {
+                        //dataGridView1.Rows.Clear();
+                        dataGridView1.DataSource = ds1.Tables["ds1"];
+                        dataGridView1.AutoResizeColumns();
+                        //dataGridView1.CurrentCell = dataGridView1[0, rownum];
+
+                        //根据列表中数据不同，显示不同颜色背景
+                        foreach (DataGridViewRow dgRow in dataGridView1.Rows)
+                        {
+                            //判断
+                            if (Convert.ToDecimal(dgRow.Cells[5].Value) > 0)
+                            {
+                                //将这行的背景色设置成Pink
+                                dgRow.DefaultCellStyle.BackColor = Color.Pink;
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
 
         #endregion
 
         #region BUTTON
         private void button1_Click(object sender, EventArgs e)
         {
-
+            SEARCHMOCMANULINE(dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
         }
 
         #endregion
