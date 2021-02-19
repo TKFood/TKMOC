1b2066c136c344a678041d6cf0ae7561c52d765b
at3158 <tk290@tkfood.com.tw>
2021-02-19 15:01:00 +0800

    SETSQL1

--- TKMOC/frmREPORTMOCCOP.cs
+++ TKMOC/frmREPORTMOCCOP.cs
@@ -67,23 +67,26 @@ namespace TKMOC
         {
             StringBuilder SB = new StringBuilder();
 
-            SB.AppendFormat(" SELECT TC053 AS '客戶',TA001 AS '製令單',TA002 AS '製令編號',TA006 AS '品號',TA034 AS '品名',TA007 AS '生產單位',TA009 AS '預計開工日',TA010 AS '預計完工日',TA014 AS '實際完工日',TA015 AS '預計產量',TA017 AS '已生產量',TA026 AS '訂單別',TA027 AS '訂單號',TA028 AS '訂單序',[OLDNUM] AS '訂單量'");
-            SB.AppendFormat(" ,(SELECT ISNULL(SUM(TA017),0) FROM [TK].dbo.MOCTA A WHERE A.TA011 IN ('Y','y') AND A.TA026=[COPTD].TD001 AND A.TA027=[COPTD].TD002 AND A.TA028=[COPTD].TD003 AND A.TA006=[COPTD].TD004  ) AS '訂單總生產量'");
-            SB.AppendFormat(" ,ISNULL(((SELECT ISNULL(SUM(TA017),0) FROM [TK].dbo.MOCTA A WHERE A.TA011 IN ('Y','y') AND A.TA026=[COPTD].TD001 AND A.TA027=[COPTD].TD002 AND A.TA028=[COPTD].TD003 AND A.TA006=[COPTD].TD004  ) -[OLDNUM]),0) AS '生產數量是否滿足訂單'");
-            SB.AppendFormat(" ,[COPTD].TD013 AS '訂單預交日'");
-            SB.AppendFormat(" ,CASE WHEN ISNULL(TA014,'')<>'' THEN DATEDIFF (DAY,[COPTD].TD013,TA014) ELSE 999 END AS '是否延遲訂單預交'");
-            SB.AppendFormat(" ,CASE WHEN ISNULL(TA014,'')<>'' THEN DATEDIFF (DAY,TA010,TA014) ELSE 999 END  AS '是否延遲製令完工'");
-            SB.AppendFormat(" ,ISNULL((TA017-TA015),0) AS '製令生產數量生否>預計生產'");
-            SB.AppendFormat(" FROM [TK].dbo.MOCTA");
-            SB.AppendFormat(" LEFT JOIN [TK].[dbo].[VCOPTDINVMD] ON [VCOPTDINVMD].TD001=TA026 AND [VCOPTDINVMD].TD002=TA027 AND [VCOPTDINVMD].TD003=TA028");
-            SB.AppendFormat(" LEFT JOIN [TK].[dbo].[COPTD] ON [COPTD].TD001=TA026 AND [COPTD].TD002=TA027 AND [COPTD].TD003=TA028");
-            SB.AppendFormat(" LEFT JOIN [TK].[dbo].[COPTC] ON [COPTC].TC001=TA026 AND [COPTC].TC002=TA027 ");
-            SB.AppendFormat(" WHERE TA001 IN ('A510','A511')");
-            SB.AppendFormat(" AND TA006 LIKE '4%'");
-            SB.AppendFormat(" AND TA009>='{0}' AND TA009<='{1}'",dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
-            SB.AppendFormat(" ORDER BY TC053,TA006 ");
-            SB.AppendFormat(" ");
-            SB.AppendFormat(" ");
+            SB.AppendFormat(@" 
+                            SELECT MD002 AS '生產線別',TC053 AS '客戶',TA001 AS '製令單',TA002 AS '製令編號',TA006 AS '品號',TA034 AS '品名',TA007 AS '生產單位',TA009 AS '預計開工日',TA010 AS '預計完工日',TA014 AS '實際完工日',TA015 AS '預計產量',TA017 AS '已生產量',TA026 AS '訂單別',TA027 AS '訂單號',TA028 AS '訂單序',[OLDNUM] AS '訂單量'
+                            ,(SELECT ISNULL(SUM(TA017),0) FROM [TK].dbo.MOCTA A WHERE A.TA011 IN ('Y','y') AND A.TA026=[COPTD].TD001 AND A.TA027=[COPTD].TD002 AND A.TA028=[COPTD].TD003 AND A.TA006=[COPTD].TD004  ) AS '訂單總生產量'
+                            ,ISNULL(((SELECT ISNULL(SUM(TA017),0) FROM [TK].dbo.MOCTA A WHERE A.TA011 IN ('Y','y') AND A.TA026=[COPTD].TD001 AND A.TA027=[COPTD].TD002 AND A.TA028=[COPTD].TD003 AND A.TA006=[COPTD].TD004  ) -[OLDNUM]),0) AS '生產數量是否滿足訂單'
+                            ,[COPTD].TD013 AS '訂單預交日'
+                            ,CASE WHEN ISNULL(TA014,'')<>'' THEN DATEDIFF (DAY,[COPTD].TD013,TA014) ELSE 999 END AS '是否延遲訂單預交'
+                            ,CASE WHEN ISNULL(TA014,'')<>'' THEN DATEDIFF (DAY,TA010,TA014) ELSE 999 END  AS '是否延遲製令完工'
+                            ,ISNULL((TA017-TA015),0) AS '製令生產數量生否>預計生產'
+                            FROM [TK].dbo.MOCTA
+                            LEFT JOIN [TK].[dbo].[VCOPTDINVMD] ON [VCOPTDINVMD].TD001=TA026 AND [VCOPTDINVMD].TD002=TA027 AND [VCOPTDINVMD].TD003=TA028
+                            LEFT JOIN [TK].[dbo].[COPTD] ON [COPTD].TD001=TA026 AND [COPTD].TD002=TA027 AND [COPTD].TD003=TA028
+                            LEFT JOIN [TK].[dbo].[COPTC] ON [COPTC].TC001=TA026 AND [COPTC].TC002=TA027 
+                            LEFT JOIN [TK].[dbo].[CMSMD] ON [CMSMD].MD001=MOCTA.TA021
+                            WHERE TA013='Y'
+                            AND  TA001 IN ('A510','A511')
+                            AND TA006 LIKE '4%'
+                            AND TA009>='{0}' AND TA009<='{1}'
+                            ORDER BY MD002,TC053,TA001,TA002  
+                             ", dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+  
 
             return SB;
 
