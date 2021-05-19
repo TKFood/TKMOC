ff14a5495f85c274a6760d6cbf6f7b5ed236f12e
at3158 <tk290@tkfood.com.tw>
2021-05-19 09:53:14 +0800

    Search

--- TKMOC/frmREPORTMOCBOM.cs
+++ TKMOC/frmREPORTMOCBOM.cs
@@ -82,7 +82,7 @@ namespace TKMOC
                                     AND TA006 LIKE '3%'
                                     AND TA003='{0}'
                                     ORDER BY TA001,TA002 
-                                    
+                                     
                                     ", dateTimePicker1.Value.ToString("yyyyMMdd"));
 
                 adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
