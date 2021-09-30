e29674536d6212d6ff725065464906bd834c522c
at3158 <tk290@tkfood.com.tw>
2021-09-30 16:59:21 +0800

    frmREPORTMOCBOMMD

--- TKMOC/frmREPORTMOCBOMMD.cs
+++ TKMOC/frmREPORTMOCBOMMD.cs
@@ -24,6 +24,7 @@ using Calendar.NET;
 using Excel = Microsoft.Office.Interop.Excel;
 using FastReport;
 using FastReport.Data;
+using TKITDLL;
 
 namespace TKMOC
 {
@@ -52,7 +53,20 @@ namespace TKMOC
             Report report1 = new Report();
             report1.Load(@"REPORT\原料-使用量差表.frx");
 
-            report1.Dictionary.Connections[0].ConnectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            report1.Dictionary.Connections[0].ConnectionString = sqlsb.ConnectionString;
+
             TableDataSource table = report1.GetDataSource("Table") as TableDataSource;
             table.SelectCommand = SQL1.ToString();
 
