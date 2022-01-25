5aef84622721f863f1324cc16d4961c18bf3dca5
at3158 <tk290@tkfood.com.tw>
2022-01-25 09:40:52 +0800

    SETSQL1

--- TKMOC/frmMOCDAILY.cs
+++ TKMOC/frmMOCDAILY.cs
@@ -417,6 +417,9 @@ namespace TKMOC
 
             StringBuilder SB = new StringBuilder();
 
+            //原料用量，品號1開頭或3開頭，單位是KG，直接加總
+            //原料用量，3開頭，單位不是KG，就把投入量*淨重/1000，加總
+
             SB.AppendFormat(@" SELECT 
                                 線別, SUBSTRING(製令單號, 1, 8) AS '日期', 品號, 品名, 規格, 製令單別, 製令單號, 生產單位, 預計產量, 生產量,桶數, 淨重, 單片重, 袋重, 袋重比, 蒸發率, 原料用量, 成品用量 / 1000 AS 成品用量, 類別, 領料是否扣袋重, 成品是否扣袋重
                                 , CASE WHEN 領料是否扣袋重 IN('Y') AND 成品是否扣袋重 IN('Y') AND  類別 NOT IN('半成品', '個', '試吃', '片', '單包', 'kg') AND(原料用量 * (1 - (蒸發率 / 100)) + (成品用量 / 1000) - (袋重比 * 原料用量)) > 0 THEN(((生產量 * 淨重 * (1 - 袋重比))) / 1000) / (原料用量 * (1 - (蒸發率 / 100)) + (成品用量 / 1000) - (袋重比 * 原料用量)) ELSE 0 END  AS '領料扣成品扣的得料率'
@@ -436,7 +439,9 @@ namespace TKMOC
 
                                 FROM(
                                 SELECT MD002 AS '線別', TA006 AS '品號', TA034 AS '品名', MB003 AS '規格', TA001 AS '製令單別', TA002 AS '製令單號', TA007 AS '生產單位', INVMB.UDF03 AS '類別', TA015 AS '預計產量', TA017 AS '生產量', INVMB.UDF07 AS '淨重', INVMB.UDF08 AS '單片重', INVMB.UDF09 AS '袋重', INVMB.UDF06 AS '蒸發率', INVMB.UDF01 AS '成品是否扣袋重', INVMB.UDF02 AS '領料是否扣袋重'
-                                , (SELECT ISNULL(SUM(TB005), 0) FROM[TK].dbo.MOCTB TB WHERE(TB.TB003 LIKE '1%' OR TB.TB003 LIKE '3%') AND TB.TB001 = MOCTA.TA001 AND TB.TB002 = MOCTA.TA002)  AS '原料用量'
+                                
+                                , ((SELECT ISNULL(SUM(TB005), 0) FROM[TK].dbo.MOCTB TB WHERE (TB.TB003 LIKE '1%' OR TB.TB003 LIKE '3%') AND TB007 IN ('kg','KG','Kg','kG') AND TB.TB001 = MOCTA.TA001 AND TB.TB002 = MOCTA.TA002)+(SELECT ISNULL(SUM(TB005*MB.UDF07/1000), 0) FROM[TK].dbo.MOCTB TB,[TK].dbo.INVMB MB WHERE MB.MB001=TB.TB003 AND (TB.TB003 LIKE '3%' AND TB007 NOT IN ('kg','KG','Kg','kG')) AND TB.TB001 = MOCTA.TA001 AND TB.TB002 = MOCTA.TA002))  AS '原料用量'
+                                
                                 ,(SELECT ISNULL(SUM(TB005 * MB.UDF07), 0) FROM[TK].dbo.MOCTB TB,[TK].dbo.INVMB MB WHERE TB.TB003=MB.MB001 AND TB.TB003 LIKE '4%' AND TB.TB001= MOCTA.TA001 AND TB.TB002= MOCTA.TA002) AS '成品用量'
                                 ,CASE WHEN INVMB.UDF08>0 AND INVMB.UDF09>0  THEN 1/(INVMB.UDF08+INVMB.UDF09)*INVMB.UDF09 ELSE 0 END AS '袋重比'
                                 ,CONVERT(decimal(16,3),(TA015/ISNULL((SELECT TOP 1 MC004 FROM [TK].dbo.BOMMC WHERE MC001=MB001),1))) AS '桶數'
