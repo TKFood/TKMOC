cfe5130100008b69d00bb9f296d84c7bf152fd66
at3158 <tk290@tkfood.com.tw>
2022-04-29 08:57:35 +0800

    add frmREPORTMOCCUT

---
+++ TKMOC/frmREPORTMOCCUT.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using Calendar.NET;
+using Excel = Microsoft.Office.Interop.Excel;
+using FastReport;
+using FastReport.Data;
+using TKITDLL;
+
+namespace TKMOC
+{
+    public partial class frmREPORTMOCCUT : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter1 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+        SqlDataAdapter adapter2 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder2 = new SqlCommandBuilder();
+        SqlDataAdapter adapter3 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder3 = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+
+        DataSet ds2 = new DataSet();
+        DataSet ds3 = new DataSet();
+        DataTable dt = new DataTable();
+        string tablename = null;
+        int rownum = 0;
+
+        SqlTransaction tran;
+
+        DataSet ds1 = new DataSet();
+        int result;
+
+        Report report1 = new Report();
+
+        public frmREPORTMOCCUT()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+        public void Search()
+        {
+            DataSet ds = new DataSet();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+                sbSql.AppendFormat(@"  
+                                    SELECT TA001 AS '製令',TA002 AS '單號',TA006 AS '品號',TA034 AS '品名',TA015 AS '生產量',TA003 AS '生產日',TA035 AS '規格',MC004 AS '標準批量',(TA015/MC004)  AS '桶數'
+                                    FROM [TK].dbo.MOCTA,[TK].dbo.BOMMC
+                                    WHERE TA006=MC001
+                                    AND (TA006 LIKE '3%' OR TA006 LIKE '4%')
+                                    AND TA021 IN ('02','03','04')
+                                    AND TA003='{0}'
+                                    ORDER BY TA001,TA002 
+                                     
+                                    ", dateTimePicker1.Value.ToString("yyyyMMdd"));
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds.Clear();
+                adapter.Fill(ds, "TEMPds1");
+                sqlConn.Close();
+
+                if (ds.Tables["TEMPds1"].Rows.Count >= 1)
+                {
+
+                    dataGridView1.DataSource = ds.Tables["TEMPds1"];
+
+                    dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 9);
+                    dataGridView1.DefaultCellStyle.Font = new Font("Tahoma", 10);
+                    dataGridView1.Columns["製令"].Width = 60;
+                    dataGridView1.Columns["單號"].Width = 100;
+                    dataGridView1.Columns["品號"].Width = 100;
+                    dataGridView1.Columns["品名"].Width = 120;
+                }
+                else
+                {
+                    dataGridView1.DataSource = null;
+                }
+
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+
+            textBox1.Text = null;
+            textBox2.Text = null;
+
+            if (dataGridView1.CurrentRow != null)
+            {
+                int rowindex = dataGridView1.CurrentRow.Index;
+                if (rowindex >= 0)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[rowindex];
+                    textBox1.Text = row.Cells["製令"].Value.ToString().Trim();
+                    textBox2.Text = row.Cells["單號"].Value.ToString().Trim();
+                  
+
+
+
+                }
+                else
+                {          
+
+                }
+            }
+        }
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        #endregion
+
+      
+    }
+}
