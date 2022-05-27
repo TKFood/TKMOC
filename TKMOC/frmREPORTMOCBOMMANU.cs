f3ca2fbf16ba287eb84cdaa3328ada741c9fc0e3
at3158 <tk290@tkfood.com.tw>
2022-05-27 14:13:30 +0800

    frmREPORTMOCBOMMANU

---
+++ TKMOC/frmREPORTMOCBOMMANU.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using Calendar.NET;
+using Excel = Microsoft.Office.Interop.Excel;
+using FastReport;
+using FastReport.Data;
+using TKITDLL;
+
+namespace TKMOC
+{
+    public partial class frmREPORTMOCBOMMANU : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter1 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+        SqlDataAdapter adapter2 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder2 = new SqlCommandBuilder();
+        SqlDataAdapter adapter3 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder3 = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+
+        DataSet ds2 = new DataSet();
+        DataSet ds3 = new DataSet();
+        DataTable dt = new DataTable();
+        string tablename = null;
+        int rownum = 0;
+
+        SqlTransaction tran;
+
+        DataSet ds1 = new DataSet();
+        int result;
+
+        Report report1 = new Report();
+
+        public frmREPORTMOCBOMMANU()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
