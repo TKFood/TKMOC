94ce12dfedc7896962afdb5db1f6ae3e346d0640
at3158 <tk290@tkfood.com.tw>
2024-03-29 17:03:19 +0800

    report

--- TKMOC/frmMOCMANULINE.cs
+++ TKMOC/frmMOCMANULINE.cs
@@ -4835,7 +4835,7 @@ namespace TKMOC
                 sbSql.Clear(); 
                 sbSqlQuery.Clear();
 
-
+                 
                 sbSql.AppendFormat(@"  
                                     SELECT TA003  AS '日期',[TA021] AS '線別號',[MD002] AS '線別',TB003 AS '品號',TB012 AS '品名',SUM(TB004)  AS '總數量',TB009  AS '入庫別'
                                     ,(SELECT  TOP 1 [MOCTA001]+' '+[MOCTA002] FROM [TKMOC].[dbo].[MOCMANULINETOATL] WHERE [TA003]=MOCTA.TA003 AND [TA021]=MOCTA.[TA021] AND [TB003]=MOCTB.TB003  AND [TB004]=SUM(MOCTB.TB004) ORDER BY [MOCTA001]+[MOCTA002] DESC) AS '製令' 
