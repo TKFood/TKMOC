e38e52292bafa65339e2cceeae41fd1eb9a0fbc7
at3158 <tk290@tkfood.com.tw>
2024-10-23 14:47:26 +0800

    SEARCHMOCMANULINE_BAKING

--- TKMOC/frmMOCMANULINEBAKING.Designer.cs
+++ TKMOC/frmMOCMANULINEBAKING.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -37,12 +38,12 @@
             this.splitContainer4 = new System.Windows.Forms.SplitContainer();
             this.splitContainer5 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
-            this.comboBox27 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label10 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label147 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -53,6 +54,7 @@
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
             this.splitContainer3.Panel2.SuspendLayout();
             this.splitContainer3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).BeginInit();
@@ -61,6 +63,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).BeginInit();
             this.splitContainer5.SuspendLayout();
             this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -135,6 +138,10 @@
             this.splitContainer3.Location = new System.Drawing.Point(0, 0);
             this.splitContainer3.Name = "splitContainer3";
             // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.dataGridView1);
+            // 
             // splitContainer3.Panel2
             // 
             this.splitContainer3.Panel2.Controls.Add(this.splitContainer4);
@@ -174,12 +181,11 @@
             this.tableLayoutPanel4.ColumnCount = 2;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel4.Controls.Add(this.comboBox27, 0, 5);
+            this.tableLayoutPanel4.Controls.Add(this.comboBox1, 0, 5);
             this.tableLayoutPanel4.Controls.Add(this.label10, 0, 0);
             this.tableLayoutPanel4.Controls.Add(this.button1, 0, 2);
-            this.tableLayoutPanel4.Controls.Add(this.dateTimePicker3, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.dateTimePicker1, 0, 1);
             this.tableLayoutPanel4.Controls.Add(this.label147, 0, 4);
-            this.tableLayoutPanel4.Controls.Add(this.button2, 0, 6);
             this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel4.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
@@ -196,18 +202,18 @@
             this.tableLayoutPanel4.Size = new System.Drawing.Size(136, 319);
             this.tableLayoutPanel4.TabIndex = 2;
             // 
-            // comboBox27
+            // comboBox1
             // 
-            this.comboBox27.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBox27.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.comboBox27.FormattingEnabled = true;
-            this.comboBox27.Items.AddRange(new object[] {
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
             "製一組",
             "製二組"});
-            this.comboBox27.Location = new System.Drawing.Point(3, 208);
-            this.comboBox27.Name = "comboBox27";
-            this.comboBox27.Size = new System.Drawing.Size(94, 24);
-            this.comboBox27.TabIndex = 123;
+            this.comboBox1.Location = new System.Drawing.Point(3, 208);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 24);
+            this.comboBox1.TabIndex = 123;
             // 
             // label10
             // 
@@ -232,16 +238,16 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dateTimePicker3
+            // dateTimePicker1
             // 
-            this.dateTimePicker3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.dateTimePicker3.CustomFormat = "yyyy/MM/dd";
-            this.dateTimePicker3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker3.Location = new System.Drawing.Point(3, 46);
-            this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(94, 27);
-            this.dateTimePicker3.TabIndex = 1;
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 46);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(94, 27);
+            this.dateTimePicker1.TabIndex = 1;
             // 
             // label147
             // 
@@ -255,15 +261,19 @@
             this.label147.Text = "線別";
             this.label147.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button2
+            // dataGridView1
             // 
-            this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(3, 243);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 30);
-            this.button2.TabIndex = 124;
-            this.button2.Text = "線別修改";
-            this.button2.UseVisualStyleBackColor = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(704, 394);
+            this.dataGridView1.TabIndex = 3;
             // 
             // frmMOCMANULINEBAKING
             // 
@@ -275,6 +285,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMOCMANULINEBAKING";
             this.Text = "frmMOCMANULINEBAKING";
+            this.Load += new System.EventHandler(this.frmMOCMANULINEBAKING_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -284,6 +295,7 @@
             this.splitContainer2.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
             this.splitContainer3.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
             this.splitContainer3.ResumeLayout(false);
@@ -294,6 +306,7 @@
             this.splitContainer5.ResumeLayout(false);
             this.tableLayoutPanel4.ResumeLayout(false);
             this.tableLayoutPanel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -309,11 +322,11 @@
         private System.Windows.Forms.SplitContainer splitContainer4;
         private System.Windows.Forms.SplitContainer splitContainer5;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
-        private System.Windows.Forms.ComboBox comboBox27;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label147;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- TKMOC/frmMOCMANULINEBAKING.cs
+++ TKMOC/frmMOCMANULINEBAKING.cs
@@ -32,12 +32,187 @@ namespace TKMOC
         SqlCommand cmd = new SqlCommand();
         DataSet ds1 = new DataSet();
 
+        string MANU = "";
+        // 宣告一個變數來儲存使用者手動選擇排序的欄位
+        string SortedColumn = string.Empty;
+        string SortedModel = string.Empty;
+
         public frmMOCMANULINEBAKING()
         {
             InitializeComponent();
         }
 
         #region FUNCTION
+        private void frmMOCMANULINEBAKING_Load(object sender, EventArgs e)
+        {
+            comboBox1load();
+        }
+
+        public void comboBox1load()
+        {
+            LoadComboBoxData(comboBox1, "SELECT MD001,MD002 FROM [TK].dbo.CMSMD WHERE MD001 IN ('08')  ", "MD002", "MD002");
+        }
+
+        public void LoadComboBoxData(ComboBox comboBox, string query, string valueMember, string displayMember)
+        {
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            using (SqlConnection connection = new SqlConnection(sqlsb.ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                comboBox.DataSource = dataTable;
+                comboBox.ValueMember = valueMember;
+                comboBox.DisplayMember = displayMember;
+            }
+        }
+
+        public void SEARCHMOCMANULINE_BAKING(string SDATES,string MANU)
+        {
+            if (MANU.Equals("吧台烘焙線"))
+            {
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+
+                sbSql.AppendFormat(@"                                      
+                                    SELECT 
+                                    [MANU] AS '線別'
+                                    ,CONVERT(varchar(100),[MANUDATE],112) AS '生產日'
+                                    ,[MOCMANULINE].[MB001] AS '品號'
+                                    ,[MOCMANULINE].[MB002] AS '品名' 
+                                    ,[MOCMANULINE].[MB003] AS '規格'
+                                    ,ALLERGEN AS '過敏原'
+                                    ,ORI AS '素別'
+                                    ,[BAR] AS '桶數'
+                                    ,[NUM] AS '數量'
+                                    ,[CLINET] AS '客戶'
+                                    ,[OUTDATE] AS '交期'
+                                    ,[TA029] AS '備註'
+                                    ,[HALFPRO] AS '半成品數量'
+                                    ,[COPTD001] AS '訂單單別'
+                                    ,[COPTD002] AS '訂單號'
+                                    ,[COPTD003] AS '訂單序號'
+                                    ,[BOX] AS '箱數'
+                                    ,[ID]
+                                    FROM [TKMOC].[dbo].[MOCMANULINE]
+                                    LEFT JOIN [TKMOC].[dbo].[ERPINVMB] ON [ERPINVMB].MB001=[MOCMANULINE].MB001
+
+                                    WHERE [MANU]='{0}' 
+                                    AND CONVERT(varchar(100),[MANUDATE],112) LIKE '{1}%'
+                                    ORDER BY [MANUDATE],[SERNO]"
+
+                                   , MANU, SDATES);
+
+                sbSql.AppendFormat(@"  ");
+
+                SEARCH_MANULINE(sbSql.ToString(), dataGridView1, SortedColumn, SortedModel);
+
+                ////SET欄位寬度
+                //if (dataGridView1.Columns.Contains("規格"))
+                //{
+                //    // 欄位存在
+                //    dataGridView1.Columns["規格"].Width = 30;
+                //}
+
+            }
+        }
+
+        public void SEARCH_MANULINE(string QUERY, DataGridView DataGridViewNew, string SortedColumn, string SortedModel)
+        {
+            SqlConnection sqlConn = new SqlConnection();
+            SqlDataAdapter SqlDataAdapterNEW = new SqlDataAdapter();
+            SqlCommandBuilder SqlCommandBuilderNEW = new SqlCommandBuilder();
+            DataSet DataSetNEW = new DataSet();
+
+            DataGridViewNew.AlternatingRowsDefaultCellStyle.BackColor = Color.PaleTurquoise;
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+                SqlDataAdapterNEW = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                SqlCommandBuilderNEW = new SqlCommandBuilder(SqlDataAdapterNEW);
+                sqlConn.Open();
+                DataSetNEW.Clear();
+                SqlDataAdapterNEW.Fill(DataSetNEW, "DataSetNEW");
+                sqlConn.Close();
+
+
+                DataGridViewNew.DataSource = null;
+
+                if (DataSetNEW.Tables["DataSetNEW"].Rows.Count >= 1)
+                {
+                    //DataGridViewNew.Rows.Clear();
+                    DataGridViewNew.DataSource = DataSetNEW.Tables["DataSetNEW"];
+                    DataGridViewNew.AutoResizeColumns();
+                    //DataGridViewNew.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                    //DataGridViewNew.CurrentCell = dataGridView1[0, rownum];
+                    //dataGridView20SORTNAME
+                    //dataGridView20SORTMODE
+
+                    if (!string.IsNullOrEmpty(SortedColumn))
+                    {
+                        if (SortedModel.Equals("Ascending"))
+                        {
+                            DataGridViewNew.Sort(DataGridViewNew.Columns["" + SortedColumn + ""], ListSortDirection.Ascending);
+                        }
+                        else
+                        {
+                            DataGridViewNew.Sort(DataGridViewNew.Columns["" + SortedColumn + ""], ListSortDirection.Descending);
+                        }
+                    }
+
+                    //SET欄位寬度
+                    if (DataGridViewNew.Columns.Contains("規格"))
+                    {
+                        // 欄位存在
+                        DataGridViewNew.Columns["規格"].Width = 100;
+                    }
+                    if (DataGridViewNew.Columns.Contains("過敏原"))
+                    {
+                        // 欄位存在
+                        DataGridViewNew.Columns["過敏原"].Width = 30;
+                    }
+                    if (DataGridViewNew.Columns.Contains("素別"))
+                    {
+                        // 欄位存在
+                        DataGridViewNew.Columns["素別"].Width = 50;
+                    }
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
 
         #endregion
 
@@ -45,9 +220,10 @@ namespace TKMOC
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            SEARCHMOCMANULINE_BAKING(dateTimePicker1.Value.ToString("yyyyMMdd"),comboBox1.Text.Trim());
         }
         #endregion
 
+       
     }
 }
