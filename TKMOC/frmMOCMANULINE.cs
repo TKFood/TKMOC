98153e5e936da096d9a4a34efeedbdf75d1ebf0e
at3158 <tk290@tkfood.com.tw>
2026-01-20 14:15:39 +0800

    REPORT

--- TKMOC/frmMOCMANULINE.cs
+++ TKMOC/frmMOCMANULINE.cs
@@ -6812,7 +6812,7 @@ namespace TKMOC
                 SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
 
                 //資料庫使用者密碼解密
-                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.Password = TKID.Decryption(sqlsb.Password); 
                 sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
 
                 String connectionString;
